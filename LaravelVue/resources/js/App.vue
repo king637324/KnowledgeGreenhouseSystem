a71f32d0b972c642df23f3a60c2984b71c2cb564
king637324 <0651095@nkust.edu.tw>
2020-12-22 16:49:34 +0800

    改用 Laravel  +  Vue 的後前端專案開發

---
+++ LaravelVue/resources/js/App.vue
@@ -0,0 +1,6 @@
+<template>
+    <main>
+        <h3>Laravel 跟 Vue 的測試</h3>
+        <button class="btn btn-primary">press me!</button>
+    </main>
+</template>
---
+++ LaravelVue/resources/js/app.js
@@ -0,0 +1,11 @@
+import Vue from 'vue';
+
+import App from './App.vue';
+
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap';
+
+new Vue({
+    el: '#app',
+    render: h => h(App)
+});
