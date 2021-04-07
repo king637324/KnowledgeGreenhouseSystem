0881f899a4bd452275ea4d9ff3f0110a01bff107
king637324 <0651095@nkust.edu.tw>
2021-04-07 15:19:18 +0800

    新增 網站線上人數計數

--- LaravelVue/resources/js/components/Foot.vue
+++ LaravelVue/resources/js/components/Foot.vue
@@ -31,6 +31,13 @@
                     <div class="p-2">
                         <h6>聯絡窗口   E-mail :  0651095@nkust.edu.tw</h6>
                         <h6>MIRDC UC 開發</h6>
+                        
+                        <h6>
+                            網站在線人數：
+                            <a href="https://vigrx.co"><img src="http://vigrx.co/count.php?c_style=115&id=1617779827" border="0"></a><br>
+                        </h6>
+                        
+
                     </div>
                     <div class="p-2">
                     </div>
